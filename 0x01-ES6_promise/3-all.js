33a48e2220dfa4f89879f45d0c08b995c878bc10
Samuel Sylvester <sasy260902@gmail.com>
2024-07-26 22:54:38 +0100

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-import { uploadPhoto, createUser } from "./utils.js";
+import { uploadPhoto, createUser } from "./utils";
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
