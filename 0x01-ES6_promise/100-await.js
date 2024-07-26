94c072c1f7aad1b3be7a578e975c830963dcd44f
Sylvester009 <sasy260902@gmail.com>
2024-07-26 19:52:46 +0100

    setup for ES6 Promises completed

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,14 @@
+/**Import uploadPhoto and createUser from utils.js
+
+Write an async function named asyncUploadUser that will call these two functions and return an object with the following format:
+
+{
+  photo: response_from_uploadPhoto_function,
+  user: response_from_createUser_function,
+}
+If one of the async function fails, return an empty object. Example:
+
+{
+  photo: null,
+  user: null,
+} */
---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,10 @@
+/**Using the following prototype
+
+function signUpUser(firstName, lastName) {
+}
+That returns a resolved promise with this object:
+
+{
+  firstName: value,
+  lastName: value,
+} */
