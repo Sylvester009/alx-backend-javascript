3d90d6f9cfa82099dc03b67bf621b9e471ee7c58
Sylvester009 <sasy260902@gmail.com>
2024-07-26 21:01:39 +0100

    revised 1-promise file

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
 function getFullResponseFromAPI(success) {
-  return promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     if (success === true) {
       resolve({
         status: 200,
         body: "Success",
       });
     } else if (success === false) {
-      reject("Error: The fake API is not working currently");
+      reject(new Error("The fake API is not working currently"));
     }
   });
 }
