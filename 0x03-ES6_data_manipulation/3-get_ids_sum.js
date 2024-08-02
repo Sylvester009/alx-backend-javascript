b7d7787d96a75dc613adee6ee0d326fc6f79645a
Samuel Sylvester <sasy260902@gmail.com>
2024-08-02 14:48:54 +0100

    update task 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,3 @@
 export default function getStudentIdsSum(students) {
-  if (!Array.isArray(students)) {
-    return 0;
-  }
-  
-  return students.reduce((accumulator, current) => accumulator + current.id, 0);
+  return students.reduce((sum, student) => sum + student.id, 0);
 }
