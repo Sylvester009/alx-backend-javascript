d07ee065f965c476811469720cab5624ee694565
Samuel Sylvester <sasy260902@gmail.com>
2024-08-02 15:38:51 +0100

    Update 5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value){
-  if(position <= 0 && position > length){
-    throw new error("out of range");
+  if(position < 0 && position >= length){
+    throw new error("Position outside range");
   }
   const buffer = new ArrayBuffer(length);
   const int8View = new DataView(buffer);
