daba27ab937d8fdb9658584d7568d9cba1f9260f
Samuel Sylvester <sasy260902@gmail.com>
2024-09-18 17:57:26 +0100

    Update 0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,10 +1,10 @@
 /**
- * create a function,
- * it is named displayMessage
- * prints argument to STDOUT
+ * show a message on the STDOUT.
+ * @param {String} arg  message to be shown.
+ * @author sylvester samuel <https://github.com/sylvester009>
  */
 const displayMessage = (arg) => {
-    console.log(arg);
+  console.log(arg);
 };
 
 module.exports = displayMessage;
