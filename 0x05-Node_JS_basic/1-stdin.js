6e9a48b66054d81c03c56ecdcbd3ec1cf00e2d53
Samuel Sylvester <sasy260902@gmail.com>
2024-09-19 00:59:13 +0100

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,11 @@
-process.stdin.setEncoding('utf-8');
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (input) => {
+process.stdin.on('data', () => {
+  const input = process.stdin.read();
 
   if (input) {
     process.stdout.write(`Your name is: ${input}`);
   }
-
-  process.stdin.end();
 });
 
 process.stdin.on('end', () => {
