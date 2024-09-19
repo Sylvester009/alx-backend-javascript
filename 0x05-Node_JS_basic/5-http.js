df1b1b854d33db9240e73d4a855df202a9dacc57
Samuel Sylvester <samsylvester09@gmail.com>
2024-09-19 01:23:35 +0100

    Create 5-http.js

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,51 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    if (req.url === '/') {
+      res.setHeader('Content-Type', 'text/plain');
+      res.write('Hello Holberton School!');
+      res.end();
+    } else if (req.url === '/students') {
+      res.setHeader('Content-Type', 'text/plain');
+
+      const dbPath = path.join(__dirname, 'database.csv');
+
+      try {
+        countStudents(dbPath)
+          .then(data => {
+            res.write('This is the list of our students\n');
+            res.write(data);
+            res.end();
+          })
+          .catch(error => {
+            res.write('Cannot load the database\n');
+            res.end();
+          });
+      } catch (error) {
+        res.write('Cannot load the database\n');
+        res.end();
+      }
+    } else {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.write('Not Found');
+      res.end();
+    }
+  } else {
+    res.statusCode = 405;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('Method Not Allowed');
+    res.end();
+  }
+});
+
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
+
+module.exports = app;
