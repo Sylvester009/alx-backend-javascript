825fca45f4660064e860712326c9c7e1029a6020
Sylvester009 <sasy260902@gmail.com>
2024-09-20 23:07:20 +0100

    fixes made to regex integration2

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,29 +1,22 @@
 const request = require('request');
 const { expect } = require('chai');
 
-describe("API integration test", () => {
-  const URL = 'http://localhost:7865';
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
 
   const testCases = [
-    {
-      path: '/',
-      expectedStatus: 200,
-      expectedBody: 'Welcome to the payment system',
-    },
-    {
-      path: '/cart/47',
-      expectedStatus: 200,
-      expectedBody: 'Payment methods for cart 47',
-    },
-    { path: '/cart/d2-44a5', expectedStatus: 404 },
+    { path: '/', expectedStatus: 200, expectedBody: 'Welcome to the payment system' },
+    { path: '/cart/47', expectedStatus: 200, expectedBody: 'Payment methods for cart 47' },
+    { path: '/cart/-47', expectedStatus: 404 },
+    { path: '/cart/d200-44a5-9de6', expectedStatus: 404 }
   ];
 
   testCases.forEach(({ path, expectedStatus, expectedBody }) => {
-    it(`GET ${path} returns correct response`, (final) => {
-      request.get(`${URL}${path}`, (_err, res, body) => {
+    it(`GET ${path} returns correct response`, (done) => {
+      request.get(`${API_URL}${path}`, (_err, res, body) => {
         expect(res.statusCode).to.equal(expectedStatus);
         if (expectedBody) expect(body).to.equal(expectedBody);
-        final();
+        done();
       });
     });
   });
