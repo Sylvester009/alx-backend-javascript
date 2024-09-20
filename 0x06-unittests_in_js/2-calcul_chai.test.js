c66f46c656d625fec0c7fbc132935b1a0f3ead2f
Sylvester009 <sasy260902@gmail.com>
2024-09-20 21:21:06 +0100

    reconstruction of 1-calcul file and test using chai

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,70 +1,70 @@
-const assert = require('assert');
-const calculateNumber = require('./1-calcul.js');
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', () => {
     describe('SUM', function () {
         it('returns 6 when a = 1.4 and b = 4.5', function () {
-            assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+            expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
           });
 
         it('returns -4 when a = -3.0 and b = -1.0', function () {
-            assert.strictEqual(calculateNumber('SUM', -3.0, -1.0), -4);
+            expect(calculateNumber('SUM', -3.0, -1.0)).to.equal(-4);
           });
 
         it('returns 4 when a = 3.0 and b = 1.0', function () {
-            assert.strictEqual(calculateNumber('SUM', 3.0, 1.0), 4);
+            expect(calculateNumber('SUM', 3.0, 1.0)).to.equal(4);
           });
 
         it('returns 0 when a = -2.0 and b = 2.0', function () {
-            assert.strictEqual(calculateNumber('SUM', -2.0, 2.0), 0);
+            expect(calculateNumber('SUM', -2.0, 2.0)).to.equal(0);
           });
 
         it('returns 0 when a = 0.0 and b = 0.0', function () {
-            assert.strictEqual(calculateNumber('SUM', 0.0, 0.0), 0);
+            expect(calculateNumber('SUM', 0.0, 0.0)).to.equal(0);
           });
     });
 
     describe('SUBTRACT', function () {
         it('returns -4 when a = 1.4 and b = 4.5', function () {
-            assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+            expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
           });
 
         it('returns -2 when a = -3.0 and b = -1.0', function () {
-            assert.strictEqual(calculateNumber('SUBTRACT', -3.0, -1.0), -2);
+            expect(calculateNumber('SUBTRACT', -3.0, -1.0)).to.equal(-2);
           });
 
         it('returns 2 when a = 3.0 and b = 1.0', function () {
-            assert.strictEqual(calculateNumber('SUBTRACT', 3.0, 1.0), 2);
+            expect(calculateNumber('SUBTRACT', 3.0, 1.0)).to.equal(2);
           });
 
         it('returns -4 when a = -2.0 and b = 2.0', function () {
-            assert.strictEqual(calculateNumber('SUBTRACT', -2.0, 2.0), -4);
+            expect(calculateNumber('SUBTRACT', -2.0, 2.0)).to.equal(-4);
           });
 
         it('returns 0 when a = 0.0 and b = 0.0', function () {
-            assert.strictEqual(calculateNumber('SUBTRACT', 0.0, 0.0), 0);
+            expect(calculateNumber('SUBTRACT', 0.0, 0.0)).to.equal(0);
           });
     });
 
     describe('DIVIDE', function () {
         it('returns 0.2 when a = 1.4 and b = 4.5', function () {
-            assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+            expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
           });
 
         it('returns Error when a = 1.4 and b = 0', function () {
-            assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+            expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
           });
 
         it('returns 4 when a = 8.0 and b = 2.0', function () {
-            assert.strictEqual(calculateNumber('DIVIDE', 8.0, 2.0), 4);
+            expect(calculateNumber('DIVIDE', 8.0, 2.0)).to.equal(4);
           });
 
         it('returns -3.5 when a = -7.0 and b = 2.0', function () {
-            assert.strictEqual(calculateNumber('DIVIDE', -7.0, 2.0), -3.5);
+            expect(calculateNumber('DIVIDE', -7.0, 2.0)).to.equal(-3.5);
           });
 
         it('returns -3.5 when a = 7.0 and b = -2.0', function () {
-            assert.strictEqual(calculateNumber('DIVIDE', 7.0, -2.0), -3.5);
+            expect(calculateNumber('DIVIDE', 7.0, -2.0)).to.equal(-3.5);
           });
     });
 });
