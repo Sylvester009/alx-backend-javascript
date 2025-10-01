d50a4fa03b947d6be30a08f3db8c3465c57864b4
Samuel Sylvester <samsylvester09@gmail.com>
2025-10-02 00:37:41 +0100

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true; // Block-scoped
-    const task2 = false; // Block-scoped
+    var task = true; // Block-scoped
+    var task2 = false; // Block-scoped
   }
 
   return [task, task2];
